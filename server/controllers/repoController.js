f895bd9dde9d4f0837b4a7374b1d933b9c14c0c9
Alex Weng <alexweng600@gmail.com>
2025-09-03 21:40:51 -0400

    cleanup repo with git ignore

---
+++ server/controllers/repoController.js
@@ -0,0 +1,34 @@
+// controllers/repoController.js
+const simpleGit = require("simple-git");
+const path = require("path");
+const fs = require("fs");
+
+const git = simpleGit();
+
+async function cloneRepoController(req, res) {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: "Repo URL is required" });
+  }
+
+  try {
+    const repoName = url.split("/").pop().replace(".git", "");
+    const localPath = path.join(__dirname, "..", "repos", repoName);
+
+    if (!fs.existsSync(localPath)) {
+      console.log(`Cloning ${url} into ${localPath} ...`);
+      await git.clone(url, localPath);
+      console.log("Repository cloned!");
+    } else {
+      console.log("Repo already exists, skipping clone.");
+    }
+
+    res.json({ message: "Repo cloned successfully", path: localPath });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to clone repo" });
+  }
+}
+
+module.exports = { cloneRepoController };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const cors = require("cors");
 const dotenv = require('dotenv');
+const { cloneRepoController } = require("../controllers/repoController");
+
+
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/',
+  cloneRepoController,
+  (req, res) => {
   res.send('Welcome to AskMyRepo 🧠');
 });
 app.listen(PORT, () => {
