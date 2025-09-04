42b261e5c66297b9686b1d2f7617e17ebe21291b
Alex Weng <alexweng600@gmail.com>
2025-09-04 17:55:40 -0400

    Update repo end point

--- server/controllers/repoController.js
+++ server/controllers/repoController.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const git = simpleGit();
 
-async function cloneRepoController(req, res) {
+async function cloneRepoController(req, res, next) {
   const { url } = req.body;
 
   if (!url) {
@@ -15,8 +15,7 @@ async function cloneRepoController(req, res) {
   try {
     const repoName = url.split("/").pop().replace(".git", "");
     const localPath = path.join(__dirname, "..", "repos", repoName);
-    res.locals.localpath = localPath;
-
+    console.log(localPath);
     if (!fs.existsSync(localPath)) {
       console.log(`Cloning ${url} into ${localPath} ...`);
       await git.clone(url, localPath);
@@ -26,6 +25,28 @@ async function cloneRepoController(req, res) {
     }
 
     console.log({ message: "Repo cloned successfully", path: localPath });
+    function getAllFiles(dirPath, arrayOfFiles = []) {
+        // Read all items in the current directory
+        const files = fs.readdirSync(dirPath);
+
+        files.forEach((file) => {
+            const fullPath = path.join(dirPath, file);
+
+            // If it’s a directory, recursively traverse
+            if (fs.statSync(fullPath).isDirectory()) {
+            arrayOfFiles = getAllFiles(fullPath, arrayOfFiles);
+            } else {
+            // If it’s a file, add to the list
+            arrayOfFiles.push(fullPath);
+            }
+        });
+        return arrayOfFiles;
+    }
+    const allFiles = getAllFiles(localPath);
+    res.locals.allFiles = allFiles;
+    // check if getAllFiles function works.
+    console.log(res.locals.allFiles);
+    
     next();
   } catch (err) {
     console.error(err);
